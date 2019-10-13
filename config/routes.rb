48af0a721224f4471763e8e8d9fb3ca956871eb5
Yuki Aida <rails.tutorial21@gmail.com>
2019-10-13 03:38:18 +0000

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,9 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   
   get '/signup',   to: 'users#new'
-  post '/signup',  to: 'users#create'
+  # post '/signup',  to: 'users#create'
   resources :users
+  resources :account_activations, only: [:edit]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
     @other_user = users(:archer)
+    @non_activated_user = users(:non_activated)
   end
   
   test "should get new" do
@@ -67,6 +68,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       delete user_path(@user)
     end
     assert_redirected_to root_url
+  end
+  
+  test "should not allow the not activated attribute" do
+    log_in_as (@non_activated_user)                                             # 非有効化ユーザーでログイン
+    assert_not @non_activated_user.activated?                                   # 有効化でないことを検証
+    get users_path                                                              # /usersを取得
+    assert_select "a[href=?]", user_path(@non_activated_user), count: 0         # 非有効化ユーザーが表示されていないことを確認
+    get user_path(@non_activated_user)                                          # 非有効化ユーザーidのページを取得
+    assert_redirected_to root_url                                               # ルートurlにリダイレクトされればtrue
   end
   
 end
